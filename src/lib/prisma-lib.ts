12f027367547143f0bfcb9a1048f1fd49bf0e37d
CodemetricsArun <ichwebdsign@gmail.com>
2024-06-13 17:23:13 +0530

    :: on Thu, Jun 13, 2024 05:23:13 PM

--- src/lib/prisma-lib.ts
+++ src/lib/prisma-lib.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => { return new PrismaClient() };
-
 declare const globalThis: { prismaGlobal: ReturnType<typeof prismaClientSingleton> } & typeof global;
 
-const appDB = globalThis.prismaGlobal ?? prismaClientSingleton()
-
+let appDB = globalThis.prismaGlobal ?? prismaClientSingleton()
 export default appDB
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = appDB;
